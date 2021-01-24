e71e91baa5028f4513f24af1e5fdc4787b5933bd
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-24 02:22:03 -0300

    Auth Reducer

--- frontend-auth/src/main/reducer.js
+++ frontend-auth/src/main/reducer.js
@@ -5,13 +5,15 @@ import {reducer as taostrReducer} from 'react-redux-toastr'
 import DashboardReducer from '../dashboard/dashboardReducer.js'
 import TabReducer from '../common/tab/tabReducer.js'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer.js'
+import AuthReducer from '../../src/auth/authReducer.js'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: taostrReducer
+    toastr: taostrReducer,
+    auth: AuthReducer
 })
 
 
