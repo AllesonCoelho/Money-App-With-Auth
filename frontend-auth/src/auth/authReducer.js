fe1eafd3ea671a99bc937d701e25eb849803d295
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-25 18:25:46 -0300

    Navbar login

--- frontend-auth/src/auth/authReducer.js
+++ frontend-auth/src/auth/authReducer.js
@@ -1,7 +1,8 @@
 const userKey = '_mymoney_user'
 
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem(userKey)),
+    //user: JSON.parse(localStorage.getItem(userKey)),
+    user: {name: 'Teste', email: 'teste@code.com.br'},
     validToken: false
 }
 
